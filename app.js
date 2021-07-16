c328a9e7e9d2be7dec8185c60409ee178af1214b
Raunak <reachtoraunak@gmail.com>
2021-07-17 00:37:16 +0530

    async/await for getProducts() using fetch locally

--- app.js
+++ app.js
@@ -14,7 +14,25 @@ const productsDOM = document.querySelector(".products-center");
 let cart = [];
 
 // getting the products
-class Products {}
+class Products {
+    async getProducts() {
+        try {
+            let result = await fetch("./products.json");
+            let data = await result.json();
+
+            let products = data.items;
+            products = products.map((item) => {
+                const { title, price } = item.fields;
+                const { id } = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return { title, price, id, image };
+            });
+            return products;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
 
 // UI products
 class UI {}
@@ -22,7 +40,9 @@ class UI {}
 // Local storage
 class Storage {}
 
-document.addEventListener("DOMContent loaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI();
     const products = new Products();
+    // get all products
+    products.getProducts().then((data) => console.log(data));
 });
