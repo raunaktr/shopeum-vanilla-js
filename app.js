61eb428b24012d8155f7242b8553be51b35e09e5
Raunak <reachtoraunak@gmail.com>
2021-07-16 17:59:51 +0530

    declaring variables in App.js :construction:

---
+++ app.js
@@ -0,0 +1,28 @@
+// Variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+//
+let cart = [];
+
+// getting the products
+class Products {}
+
+// UI products
+class UI {}
+
+// Local storage
+class Storage {}
+
+document.addEventListener("DOMContent loaded", () => {
+    const ui = new UI();
+    const products = new Products();
+});
