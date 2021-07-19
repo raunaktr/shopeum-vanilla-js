9c8a71859bedce94a59cc8d545d6ac91f21d25ed
Raunak <reachtoraunak@gmail.com>
2021-07-19 16:31:01 +0530

    integrating contenful CMS :zap:

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-// Variables
+/**
+ * Contentful - CMS
+ * using directly in the browser
+ * @type first request was made here
+ */
+const client = contentful.createClient({
+    // This is the space ID. A space is like a project folder in Contentful terms
+    space: process.env.SPACE,
+    // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+    accessToken: process.env.ACCESS_TOKEN,
+});
 
+// Variables
 const cartBtn = document.querySelector(".cart-btn");
 const closeCartBtn = document.querySelector(".close-cart");
 const clearCartBtn = document.querySelector(".clear-cart");
@@ -19,10 +30,20 @@ let buttonsDOM = [];
 class Products {
     async getProducts() {
         try {
-            let result = await fetch("./products.json");
-            let data = await result.json();
+            let contentful = await client.getEntries({
+                content_type: "shopeumProducts",
+            });
 
-            let products = data.items;
+            /**
+             *
+             * this was earlier used for fetching JSON locally
+             * let result = await fetch("./products.json");
+             * let data = await result.json();
+             * let products = data.items;
+             *
+             */
+
+            let products = contentful.items;
             products = products.map((item) => {
                 const { title, price } = item.fields;
                 const { id } = item.sys;
